fbc90ebdcaca4193aef6185a94e0b340be6d49a6
chandan-quadb <chandan.tech@quadbtech.com>
2024-12-24 15:17:07 +0530

    change some commands

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-
- 
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { createCanister } from "./createcanistermain.js";
 import { createAgent } from "./createAgent.js";
-
+import shell from "shelljs";
 yargs(hideBin(process.argv))
   // Command for creating a new canister
   .command("deploy", "Create new canister", {}, async () => {
@@ -13,6 +11,7 @@ yargs(hideBin(process.argv))
       console.log("Starting the canister creation process...");
       await createCanister();
       console.log("Canister creation process completed successfully.");
+    
     } catch (error) {
       console.error(
         "An error occurred during the canister creation process:",
@@ -37,8 +36,9 @@ yargs(hideBin(process.argv))
   .command("install", "Create a new agent", {}, async () => {
     try {
       console.log("Starting the agent creation process...");
-      await createCanister2();
+      await createCanister();
       console.log("Agent creation process completed successfully.");
+     
     } catch (error) {
       console.error(
         "An error occurred during the agent creation process:",
