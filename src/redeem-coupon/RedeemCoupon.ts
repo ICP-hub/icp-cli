44fc687671564402471ba27a2c3a89784358f515
anishsinghQB <anish.singh@quadbtech.com>
2025-02-03 14:44:35 +0530

    fix canister id issue : correct code for use the correct canister id for redeeem the cycles

--- src/redeem-coupon/RedeemCoupon.ts
+++ src/redeem-coupon/RedeemCoupon.ts
@@ -1,27 +1,42 @@
-import { idlFactory } from "./idlFactory";
-const { Actor, HttpAgent } = require('@dfinity/agent');
+import { Principal } from "@dfinity/principal";
+import { HttpAgent, toHex } from "@dfinity/agent";
+import { IDL } from "@dfinity/candid";
 
-
-export async function redeemFaucetCoupon(couponCode: any, toSubaccount: any, faucetPrincipal: any, identity : any) {
+export async function redeemFaucetCoupon(
+  couponCode: string,
+  toSubaccount: Uint8Array | null,
+  canisterId: Principal,
+  identity: any
+) {
   try {
     const agent = new HttpAgent({ identity });
 
-    const faucetActor = Actor.createActor(idlFactory, {
-      agent,
-      canisterId: faucetPrincipal,
+    const args = IDL.encode(
+      [
+        IDL.Text,
+        IDL.Record({
+          owner: IDL.Principal,
+          subaccount: IDL.Opt(IDL.Vec(IDL.Nat8)),
+        }),
+      ],
+      [
+        couponCode,
+        {
+          owner: identity.getPrincipal(),
+          subaccount: toSubaccount ? [Array.from(toSubaccount)] : [],
+        },
+      ]
+    );
+
+    const response : any = await agent.call(canisterId, {
+      methodName: "redeem_to_cycles_ledger",
+      arg: new Uint8Array(args),
     });
-    console.log("faucetActor : ",faucetActor);
 
-    const response = await faucetActor.redeem_to_cycles_ledger(
-      couponCode,
-    { owner: identity.getPrincipal(), subaccount : toSubaccount });
-
-    console.log(`Coupon redeemed successfully!`);
-    console.log(`Cycles: ${response.cycles}`);
-
+    console.log("Faucet Actor Response:", response);
     return response;
   } catch (error) {
-    console.error('Error redeeming coupon:', error);
+    console.error("Error redeeming coupon:", error);
     throw error;
   }
 }
--- src/redeem-coupon/faucetCycles.ts
+++ src/redeem-coupon/faucetCycles.ts
@@ -1,12 +1,14 @@
 import { Secp256k1KeyIdentity } from "@dfinity/identity-secp256k1";
 import { redeemFaucetCoupon } from "./RedeemCoupon";
+import { Principal } from "@dfinity/principal";
 
 
 export const faucerCoupon = async () => {
     const identity = Secp256k1KeyIdentity.fromSeedPhrase("earth input twelve fog improve voyage life ill atom turkey inside one loop digital valley miracle torch hedgehog oak time glove liberty fabric orange");
     const couponCode = '9EC33-9FFBF-9A4CA';
-    const toSubaccount = identity?.getPrincipal();
-    const faucetPrincipal: any = identity?.getPrincipal();
+    const toSubaccount : any = identity?.getPrincipal();
+    const faucetPrincipal: Principal = Principal.fromUint8Array(new Uint8Array([0, 0, 0, 0, 1, 112, 0, 196, 1, 1]));
+
 
     try {
         const result = await redeemFaucetCoupon(couponCode, toSubaccount, faucetPrincipal, identity);
